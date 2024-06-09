673826a0a7f66b4b4b740a56f4ed80705c14a0a1
giapdq04 <167546581+giapdq04@users.noreply.github.com>
2024-06-09 20:20:40 +0700

    sửa lại phương thức "lấy danh mục" 🚀🚀🚀

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -5,17 +5,9 @@ exports.layDanhSach = async (req, res) => {
         const data = await Category.find()
 
         if (data) {
-            res.json({
-                status: 200,
-                message: "Lấy danh sách thành công",
-                data: data
-            })
+            res.json(data)
         } else {
-            res.json({
-                status: 400,
-                message: "Lấy danh sách thất bại",
-                data: []
-            })
+            res.json([])
         }
     } catch (error) {
         res.status(400).json({
