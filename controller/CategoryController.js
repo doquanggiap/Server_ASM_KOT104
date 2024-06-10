ce6894ebd753a0a90866262f752cf5c5dd74eeb6
giapdq04 <167546581+giapdq04@users.noreply.github.com>
2024-06-10 22:46:30 +0700

    update 🚀🚀🚀

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -2,9 +2,24 @@ const { Category } = require("../model/Category")
 
 exports.layDanhSach = async (req, res) => {
     try {
-        const data = await Category.find()
+        let data = await Category.find()
 
         if (data) {
+            // Tìm index của item có cateName là 'Popular'
+            const popularIndex = data.findIndex(item => item.cateName === 'Popular');
+
+            // Nếu tìm thấy item 'Popular'
+            if (popularIndex !== -1) {
+                // Lấy ra item 'Popular'
+                const popularItem = data[popularIndex];
+
+                // Xóa item 'Popular' khỏi vị trí hiện tại
+                data.splice(popularIndex, 1);
+
+                // Đặt item 'Popular' lên đầu danh sách
+                data.unshift(popularItem);
+            }
+
             res.json(data)
         } else {
             res.json([])
